5dc53c726d4b71fb9089beb4b802787c0222bd52
thagana <samuelmthwa79@gmail.com>
2022-10-13 23:23:30 +0200

    notifications

--- src/views/PayClient/Pay.tsx
+++ src/views/PayClient/Pay.tsx
@@ -8,6 +8,8 @@ import Notification from "antd/es/notification";
 
 import "./Pay.scss";
 import Server from "../../networking/server";
+import Loading from "../../components/Loading";
+import ThankYou from "../../components/ThankYou";
 
 type Props = {
   location: {
@@ -31,14 +33,22 @@ type Reference = {
   transaction: string;
   trxref: string;
 };
+
+type SERVER_STATE = "IDLE" | "LOADING" | "ERROR" | "SUCCESS";
+
 export default function Pay() {
   const [amount, setAmount] = React.useState(0);
-
+  const [serverState, setServerStates] =
+    React.useState<SERVER_STATE>("IDLE");
   const navigate = useNavigate();
   const location = useLocation();
 
   const handleAmount = (e: any) => {
     setAmount(e.target.value);
+  };
+
+  const retryPayment = () => {
+    navigate("/search");
   };
 
   const config = {
@@ -55,23 +65,26 @@ export default function Pay() {
   const onSuccess = (reference: Reference) => {
     (async () => {
       try {
+        setServerStates("LOADING");
         const { reference: token } = reference;
         const response = await Server.verifyPayment({
           token,
           id: location.state.userId,
         });
         if (!response.data.success) {
+          setServerStates("ERROR");
           Notification.error({
             message: response.data.message,
           });
         } else {
+          setServerStates("SUCCESS");
           Notification.success({
             message: response.data.message,
           });
-          navigate("/");
         }
       } catch (error) {
         console.log(error);
+        setServerStates("ERROR");
         Notification.error({
           message: "Something went wrong please try again later",
         });
@@ -98,32 +111,54 @@ export default function Pay() {
   return (
     <TemplateWrapper defaultIndex="1">
       <div className="pay-container">
-        <div className="header">
-          {location.state.firstName + " " + location.state.lastName}
-        </div>
-        <div className="image-container">
-          <img src={location.state.avatar} className="image" alt="User" />
-        </div>
-        <div>
-          <div className="amount-enter">
-            <div className="input-group mb-3">
-              <span className="input-group-text">ZAR</span>
-              <input
-                type="number"
-                className="form-control"
-                value={amount}
-                onChange={handleAmount}
-                aria-label="Amount (to the nearest dollar)"
-              />
-              <span className="input-group-text">.00</span>
+        {serverState === "IDLE" && (
+          <>
+            <div className="header">
+              {location.state.firstName + " " + location.state.lastName}
             </div>
+            <div className="image-container">
+              <img src={location.state.avatar} className="image" alt="User" />
+            </div>
+            <div>
+              <div className="amount-enter">
+                <div className="input-group mb-3">
+                  <span className="input-group-text">ZAR</span>
+                  <input
+                    type="number"
+                    className="form-control"
+                    value={amount}
+                    onChange={handleAmount}
+                    aria-label="Amount (to the nearest dollar)"
+                  />
+                  <span className="input-group-text">.00</span>
+                </div>
+              </div>
+            </div>
+            <div className="d-grid gap-2">
+              <Button type="primary" clickHandler={handleClick} size="large">
+                Appreciate
+              </Button>
+            </div>
+          </>
+        )}
+        {serverState === "LOADING" && (
+          <div className="loader">
+            <Loading />
           </div>
-        </div>
-        <div className="d-grid gap-2">
-          <Button type="primary" clickHandler={handleClick} size="large">
-            Appreciate
-          </Button>
-        </div>
+        )}
+        {serverState === "ERROR" && (
+          <div>
+            <div>Something went wrong please try again</div>
+            <Button type="primary" clickHandler={retryPayment}>
+              Try Again
+            </Button>
+          </div>
+        )}
+        {serverState === "SUCCESS" && (
+          <div>
+            <ThankYou />
+          </div>
+        )}
       </div>
     </TemplateWrapper>
   );
